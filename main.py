e5f5ebabfa4cd5e7247536a0071fc32ebad16ec7
Gerson Ramirez <ram22281@uvg.edu.gt>
2025-03-26 19:47:14 -0600

    chore: add main files to start implementation

---
+++ main.py
@@ -0,0 +1,45 @@
+"""
+Problema: Máximo Subarreglo (Kadane + DaC)
+Autores: Gerson Ramírez
+         Diego Valenzuela
+Descripción: Este proyecto explora el problema del Máximo Subarreglo mediante dos enfoques 
+             algorítmicos: Programación Dinámica (Kadane) y Divide y Conquista.
+Curso: Análisis y Diseño de Algoritmos - UVG 2025
+"""
+
+import time
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+# TODO: Implementar el algoritmo de Kadane que encuentre la suma máxima de un subarreglo contiguo
+def kadane(arr):
+    pass
+
+# TODO: Implementar la función auxiliar para encontrar la suma máxima que cruza el punto medio
+def max_crossing_subarray(arr, low, mid, high):
+    pass
+
+# TODO: Implementar el algoritmo de Divide y Conquista para el máximo subarreglo
+def max_subarray_dac(arr, low, high):
+    pass
+
+# TODO: Función envoltorio para facilitar el llamado del algoritmo DaC
+def find_max_subarray(arr):
+    pass
+
+# TODO: Función para medir tiempo de ejecución de un algoritmo sobre múltiples casos de prueba
+def measure_time(algorithm, test_cases):
+    pass
+
+# TODO: Función para generar casos de prueba con arreglos aleatorios
+def generate_test_cases(num_cases=30, min_size=100, max_size=10000):
+    pass
+
+# TODO: Función para graficar el rendimiento de ambos algoritmos
+def plot_performance(sizes, kadane_times, dac_times):
+    pass
+
+# TODO: Función principal para ejecución del programa
+if __name__ == "__main__":
+    pass
